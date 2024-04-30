5e41678c81fb6fa3fcb54217fe524922cb35662f
pegause0 <115538545+pegause0@users.noreply.github.com>
2024-04-30 11:52:48 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.task-web-nine.vercel.app],
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials: true,
   })
